d998c86fe0474f4487d5ba75663d4d2429bbc1c4
Mike Maxson <thepaladinrevenant@gmail.com>
2019-09-16 18:12:05 -0400

    Modal does not update if it is closed

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,18 +3,26 @@ import React from "react";
 import Backdrop from "../Backdrop/Backdrop";
 import classes from "./Modal.module.css";
 
-const modal = (props) => (
-  <>
-    <Backdrop clickHandler={props.clickHandler} show={props.show} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}>
-      {props.children}
-    </div>
-  </>
-);
+class Modal extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  render() {
+    return (
+      <>
+        <Backdrop clickHandler={this.props.clickHandler} show={this.props.show} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}>
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Modal;
